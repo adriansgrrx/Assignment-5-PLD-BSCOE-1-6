45a127504e31e32dcf8b7ce95b03da726ee87a8c
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-14 22:23:34 +0800

    PROGRAM NEARLY TO BE COMPLETE

--- Program2.py
+++ Program2.py
@@ -3,20 +3,30 @@
 # Find the lowest number using only if-else statement.
 # Display the lowest number.
 
-# *********PROGRAM FLOW************
-# 1. Ask for the first number.
-# 2. Ask for the second number.
-# 3. Ask for the third number.
-# 4. Define a variable for indication.
+#Welcome statement
+print("\nHey there!, please enter a number of your choice!")
 
+# **********PROGRAM FLOW*****************
+# 1. Ask for the first number.
+first_number = int(input("\nEnter your first number: "))
+
+# 2. Ask for the second number.
+second_number = int(input("Enter your second number: "))
+
+# 3. Ask for the third number.
+third_number = int(input("Enter your third number: "))
+
+#4. Define a variable for indication.
+smallest_num = 0
 
 # if-else statements section
+
 # 5. if statement for the first input and present an inequalities.
+if first_number < second_number and first_number < third_number :
     #Variable for the first number if it is the smallest
+    smallest_num = first_number
 # 6. else statement for the second input.
     # present an inequality
 # 7. else statement for the third input.
         #proceeding to the last variable for the third number if it is the smallest.
-# 8. Display
-
-
+#  8. Display
