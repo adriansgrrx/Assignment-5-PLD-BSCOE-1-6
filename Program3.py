f03003781b2d2afe6ce3cd3d9683f1b6bf2b99b8
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-15 00:07:03 +0800

    PROGRAM FLOW: Finished

---
+++ Program3.py
@@ -0,0 +1,20 @@
+# Life stages
+# Create a program that ask for an age of a person
+# Display the life stage of the person
+# Rules:
+# 0-12: Kid
+# 13-17: Teen
+# 18: Debut
+# 19 above: Adult
+
+# Welcome statement
+
+# Ask for the age.
+
+# ****if-else section****
+
+# test the input if classified as Kid
+    # test the input if classified as Teen
+        # test the input if classified as Debut
+            # proceed directly to adult classification
+# add ending/closing statement by using print
