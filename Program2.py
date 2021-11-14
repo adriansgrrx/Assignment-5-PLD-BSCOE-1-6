e0d05c13dced634f2c8a338926c066356b5241a8
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-14 21:17:21 +0800

    PROGRAM FLOW: Finished

---
+++ Program2.py
@@ -0,0 +1,22 @@
+# Find the lowest number 
+# Create a program that asks three numbers
+# Find the lowest number using only if-else statement.
+# Display the lowest number.
+
+# *********PROGRAM FLOW************
+# 1. Ask for the first number.
+# 2. Ask for the second number.
+# 3. Ask for the third number.
+# 4. Define a variable for indication.
+
+
+# if-else statements section
+# 5. if statement for the first input and present an inequalities.
+    #Variable for the first number if it is the smallest
+# 6. else statement for the second input.
+    # present an inequality
+# 7. else statement for the third input.
+        #proceeding to the last variable for the third number if it is the smallest.
+# 8. Display
+
+
