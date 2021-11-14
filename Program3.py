770267d8b141d280a1d9e757d75eb69a38f8eb2e
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2021-11-15 01:12:48 +0800

    PROGRAM FINALIZED: Executed

--- Program3.py
+++ Program3.py
@@ -8,13 +8,25 @@
 # 19 above: Adult
 
 # Welcome statement
+print("\nLet's see what's your life stage!")
 
 # Ask for the age.
+age = int(input("\nPlease enter your age: "))
 
 # ****if-else section****
-
 # test the input if classified as Kid
+if age >= 0 and age <= 12:
+    print(f"\nYour age is {age} years old!\nLife Stage Classification: You're a Kid!")
+else:
     # test the input if classified as Teen
+    if age >= 13 and age <= 17:
+        print(f"\nYour age is {age} years old!\nLife Stage Classification: You're a Teen!")
+    else:
         # test the input if classified as Debut
-            # proceed directly to adult classification
+        if age == 18:
+            print(f"\nYour age is {age} years old!\nLife Stage Classification: Debut!")
+        else:
+            # proceed directly as an adult classification
+            print(f"\nYour age is {age} years old!\nLife Stage Classification: You're an Adult!")
 # add ending/closing statement by using print
+print("\nGreat!\n")      
